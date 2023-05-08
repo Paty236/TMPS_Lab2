f3d6603cb0d11285d921086f8291c3d3b2454c74
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-05-08 23:04:17 +0300

    Add files via upload

---
+++ Bridge/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+//definim o interfață IEngine care specifică o metodă GetConsumption() care
+//returnează consumul unui motor în litri/100km. Această interfață va fi implementată
+//de clasele JZ2 și RB26, care sunt tipurile de motor disponibile pentru modelele de mașini Supra și GTR.
+public interface IEngine
+{
+    double GetConsumption();
+}
+
+//definim clasa JZ2, care reprezintă un tip de motor disponibil pentru modelul de mașină Supra.
+//Clasa implementează interfața IEngine și suprascrie metoda GetConsumption(),
+//returnează consumul specific pentru acest motor.
+public class JZ2 : IEngine
+{
+    public double GetConsumption()
+    {
+        return 11.6;
+    }
+}
+
+//definim clasa RB26, care reprezintă un tip de motor disponibil pentru modelul de mașină GTR.
+//Clasa implementează interfața IEngine și suprascrie metoda GetConsumption(),
+//returnează consumul specific pentru acest motor.
+public class RB26 : IEngine
+{
+    public double GetConsumption()
+    {
+        return 12.3;
+    }
+}
+
+//definim clasa abstractă Car, care va fi moștenită de clasele concrete Supra și GTR.
+//Clasa definește o variabilă protected _engine de tip IEngine și un constructor care
+//primește o instanță de IEngine și o atribuie variabilei _engine.
+//Această clasă abstractă definește metoda abstractă DisplayConsumption(), c
+//are va fi implementată de clasele de mai jos.
+public abstract class Car
+{
+    protected IEngine _engine;
+
+    public Car(IEngine engine)
+    {
+        _engine = engine;
+    }
+
+    public abstract void DisplayConsumption();
+}
+
+//definim clasa Supra, care este un tip de mașină cu un motor de tip JZ2. Această clasă
+//moștenește clasa abstractă Car și implementează metoda DisplayConsumption(),
+//care afișează consumul specific al mașinii Supra.
+public class Supra : Car
+{
+    public Supra(IEngine engine)
+        : base(engine)
+    { }
+
+    public override void DisplayConsumption()
+    {
+        Console.WriteLine("Supra consumption: " + _engine.GetConsumption());
+    }
+}
+
+//definim clasa GTR, care este un tip de mașină cu un motor de tip RB26. Această clasă
+//moștenește clasa abstractă Car și implementează metoda DisplayConsumption(),
+//care afișează consumul specific al mașinii GTR.
+public class GTR : Car
+{
+    public GTR(IEngine engine)
+        : base(engine)
+    { }
+
+    public override void DisplayConsumption()
+    {
+        Console.WriteLine("GTR consumption: " + _engine.GetConsumption());
+    }
+}
---
+++ Bridge/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Acest program creează instanțe ale claselor JZ2 și RB26 care implementează interfața IEngine,
+//apoi creează instanțe ale claselor Supra și GTR care extind clasa abstractă Car.
+//Se apelează metoda DisplayConsumption() pentru fiecare instanță de Supra și GTR,
+//și afișează consumul fiecărui motor.
+class Program
+{
+    static void Main(string[] args)
+    {
+        JZ2 jz2 = new JZ2();
+        RB26 rb26 = new RB26();
+        Supra supra1 = new Supra(jz2);
+        Supra supra2 = new Supra(rb26);
+        GTR gtr1 = new GTR(rb26);
+        GTR gtr2 = new GTR(jz2);
+
+        supra1.DisplayConsumption();
+        supra2.DisplayConsumption();
+        gtr1.DisplayConsumption();
+        gtr2.DisplayConsumption();
+
+        Console.ReadLine();
+    }
+}
