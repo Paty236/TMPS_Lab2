f6eb26f6ccef49c04041fedd7284c66223fd1c85
Paty Reitman <97533555+Paty236@users.noreply.github.com>
2023-05-12 21:09:50 +0300

    Add files via upload

---
+++ Proxy./Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+//definim interfeța ILogger, care specifică o metodă LogThis ce primește două argumente:
+//un text care trebuie logat și un nivel pentru logare
+interface ILogger
+{
+    void LogThis(string text, int level);
+}
+
+//Clasa RealLogger implementează interfața ILogger și este pentru scrierea log-urilor în fișier.
+//Aceasta primește calea către fișierul de logare în constructor și, în metoda LogThis,
+//afișează un mesaj pe consolă, și scrie textul de logare în fișier folosind clasa StreamWriter.
+class RealLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public RealLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void LogThis(string text, int level)
+    {
+        Console.WriteLine($"Writing into {_filePath} the following:");
+        Console.WriteLine($"\tLevel {level}: {text}");
+        using StreamWriter writer = new StreamWriter(_filePath, true);
+        writer.WriteLine($"Level {level}: {text}");
+    }
+}
+
+//Clasa ProxyLogger este o implementare a design pattern-ului Proxy, care implementează interfața ILogger.
+//Aceasta primește calea către fișierul de logare în constructor și declară o instanță a clasei RealLogger.
+//Metoda LogThis a clasei ProxyLogger verifică nivelul de logare și, dacă este mai mare sau egal cu o anumită
+//valoare minimă (minLevel), atunci apelul metodei LogThis este către instanța de RealLogger,care se ocupă
+//de scrierea efectivă a log-ului. Dacă nivelul este prea mic, metoda LogThis a clasei ProxyLogger nu face nimic.
+class ProxyLogger : ILogger
+{
+    private readonly RealLogger _realLogger;
+    private const int minLevel = 5;
+
+    public ProxyLogger(string filePath)
+    {
+        _realLogger = new RealLogger(filePath);
+    }
+
+    public void LogThis(string text, int level)
+    {
+        if (level >= minLevel)
+            _realLogger.LogThis(text, level);
+    }
+}
---
+++ Proxy./main.cs
@@ -0,0 +1,17 @@
+﻿//se creează o instanță a clasei RealLogger cu fișierul de jurnalizare "logfile.txt".
+//Apoi, se apelează metoda LogThis() de două ori pe instanța realLogger, prima dată cu mesajul
+//"the message 1" și nivelul de logare 2, iar a doua oară cu mesajul "the message 2" și nivelul de logare 5.
+//Ambele mesaje se scriu în fișierul "logfile.txt", deoarece nu există restricții privind nivelul
+//de logare pentru instanța realLogger.
+RealLogger realLogger = new RealLogger("logfile.txt");
+realLogger.LogThis("the message 1", 2);
+realLogger.LogThis("the message 2", 5);
+
+Console.WriteLine("\nUsing proxy logger\n");
+ProxyLogger proxyLogger = new ProxyLogger("logfile.txt");
+proxyLogger.LogThis("the message 3", 2);
+proxyLogger.LogThis("the message 4", 4);
+proxyLogger.LogThis("the message 5", 5);
+proxyLogger.LogThis("the message 6", 6);
+
+Console.ReadKey();
